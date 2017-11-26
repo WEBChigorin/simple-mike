6cb4718ea682cf591653ba5101082255737c6681
Chigorin Mikhail <webchigorin@gmail.com>
2017-11-26 16:05:49 +0300

    add gulp-sass and gulp-browser.sync

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var browserSync = require('browser-sync');
+
+gulp.task('watch', ['sass'], function(){
+
+  browserSync.init({
+    server: '.'
+});
+
+gulp.watch('./scss/**/*.scss', ['sass'], browserSync.reload);
+gulp.watch('./*.html').on('change', browserSync.reload);
+gulp.watch('./js/**/*.js', browserSync.reload);
+
+});
+
+
+//Compile Sass into CSS & inject into browsers
+
+gulp.task('sass', function(){
+  return gulp.src('./scss/**/*.scss')
+  .pipe(sass().on('error', sass.logError))
+  .pipe(gulp.dest('./css'))
+  .pipe(browserSync.stream());
+});
+
+//default will also watch
+gulp.task('default', ['watch']);
